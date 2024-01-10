496e9a24bdb22f58c7d4eb15efd856240e99f82b
sourceTree <sourcetree@mail.com>
2024-01-09 20:59:10 -0700

    16 - Using JUnit lifecycle hook annotations

--- test/MathUtilsTest.java
+++ test/MathUtilsTest.java
@@ -1,24 +1,27 @@
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class MathUtilsTest {
+    MathUtils mathUtils;
+    
+    @BeforeEach
+    void init(){
+        mathUtils = new MathUtils();
+    }
     @Test
-    void testAdd(){
-        MathUtils mathUtils = new MathUtils();
+    void testAdd(){        
         int expect = 2;
         int reality = mathUtils.add(1, 1);
-
         assertEquals(expect, reality, "The add method should add two numbers");
     }
     @Test
-    void testComputeCircleRadius(){
-        MathUtils mathUtils = new MathUtils();
+    void testComputeCircleRadius(){        
         assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area");
     }
     @Test
-    void testDivide(){
-        MathUtils mathUtils = new MathUtils();        
+    void testDivide(){        
         assertThrows(ArithmeticException.class, ()-> mathUtils.divide(1, 0), "divide by zero should throw");
     }
 }
