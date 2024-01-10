c3bd00fcee5af2861d4322528eb6225fc9e42b14
sourceTree <sourcetree@mail.com>
2024-01-09 21:15:17 -0700

    18 - Changing default TestInstance behavior

--- test/MathUtilsTest.java
+++ test/MathUtilsTest.java
@@ -4,12 +4,15 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
+@TestInstance(Lifecycle.PER_CLASS)
 public class MathUtilsTest {
     MathUtils mathUtils;
     
     @BeforeAll
-    static void beforeAllInit(){
+    void beforeAllInit(){
         System.out.println("This needs to run before all");
     }
     @BeforeEach
