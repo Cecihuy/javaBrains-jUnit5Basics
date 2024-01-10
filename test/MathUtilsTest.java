f89e56b3a3d72d179140012c4f013d0ce6a69aef
sourceTree <sourcetree@mail.com>
2024-01-09 20:21:02 -0700

    12 - Test driven development with JUnit

--- test/MathUtilsTest.java
+++ test/MathUtilsTest.java
@@ -4,11 +4,16 @@ import org.junit.jupiter.api.Test;
 
 public class MathUtilsTest {
     @Test
-    void test(){
+    void testAdd(){
         MathUtils mathUtils = new MathUtils();
-        int expect = 1;
+        int expect = 2;
         int reality = mathUtils.add(1, 1);
 
         assertEquals(expect, reality, "The add method should add two numbers");
+    }
+    @Test
+    void testComputeCircleRadius(){
+        MathUtils mathUtils = new MathUtils();
+        assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area");
     }
 }
